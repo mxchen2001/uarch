63c3f28163fe7b24fc761dffa6e8316b8a28a2ec
Xige Michael Chen <mxchen2001@gmail.com>
2022-01-27 20:16:55 -0600

    Complete ALU (Need Testing)

--- Problem Sets/PS1b.v
+++ Problem Sets/PS1b.v
@@ -149,8 +149,85 @@ module MUX4_16 (i0, i1, i2, i3, s, o);
    input [1:0] s;
    output [15:0] o;
 
-   
-   
+   wire [3:0] select_line;
+   wire s0bar, s1bar;
+   not_nand nn1(s[0], s1bar), 
+            nn2(s[1], s2bar);
+
+   and_nand select1(s1bar, s0bar, select_line[0]),
+            select2(s1bar, s[0], select_line[1]),
+            select3(s[1], s0bar, select_line[2]),
+            select4(s[1], s[0], select_line[3]);
+
+   wire [15:0] inter1, inter2, inter3, inter4, inter5, inter6, inter7, inter8;
+
+   genvar i;
+
+   generate
+      for (i = 0; i < 16; i = i + 1) begin
+         nand2$   n0_i(inter1[i], i0[i], select_line[0]),
+                  n1_i(inter2[i], i1[i], select_line[1]),
+                  n2_i(inter3[i], i2[i], select_line[2]),
+                  n3_i(inter4[i], i3[i], select_line[3]);
+      end
+
+      for (i = 0; i < 16; i = i + 1) begin
+         nand2$   n4_i(inter5[i], inter1[i], inter2[i]),
+                  n5_i(inter6[i], inter3[i], inter4[i]);
+      end
+
+      for (i = 0; i < 16; i = i + 1) begin
+         not_nand nn1_i(inter5[i], inter7[i]), 
+                  nn2_i(inter6[i], inter8[i]);
+      end
+
+      for (i = 0; i < 16; i = i + 1) begin
+         nand2$   n6_i(o[i], inter7[i], inter8[i]);
+      end
+   endgenerate
+endmodule
+
+module MUX4_8 (i0, i1, i2, i3, s, o);
+   input [7:0] i0, i1, i2, i3;
+   input [1:0] s;
+   output [7:0] o;
+
+   wire [3:0] select_line;
+   wire s0bar, s1bar;
+   not_nand nn1(s[0], s1bar), 
+            nn2(s[1], s2bar);
+
+   and_nand select1(s1bar, s0bar, select_line[0]),
+            select2(s1bar, s[0], select_line[1]),
+            select3(s[1], s0bar, select_line[2]),
+            select4(s[1], s[0], select_line[3]);
+
+   wire [7:0] inter1, inter2, inter3, inter4, inter5, inter6, inter7, inter8;
+
+   genvar i;
+
+   generate
+      for (i = 0; i < 8; i = i + 1) begin
+         nand2$   n0_i(inter1[i], i0[i], select_line[0]),
+                  n1_i(inter2[i], i1[i], select_line[1]),
+                  n2_i(inter3[i], i2[i], select_line[2]),
+                  n3_i(inter4[i], i3[i], select_line[3]);
+      end
+
+      for (i = 0; i < 8; i = i + 1) begin
+         nand2$   n4_i(inter5[i], inter1[i], inter2[i]),
+                  n5_i(inter6[i], inter3[i], inter4[i]);
+      end
+
+      for (i = 0; i < 8; i = i + 1) begin
+         not_nand nn1_i(inter5[i], inter7[i]), 
+                  nn2_i(inter6[i], inter8[i]);
+      end
+
+      for (i = 0; i < 8; i = i + 1) begin
+         nand2$   n6_i(o[i], inter7[i], inter8[i]);
+      end
+   endgenerate
 endmodule
 
 // Slices of ALU
@@ -185,16 +262,20 @@ module ADD_SAT_slice (a, b, s, out);
    input [1:0] s;    
    output [15:0] out;
 
+   wire [15:0] output_line;
+   wire [15:0] sum
    wire [16:0] carry;
    wire ol, oh;
+
+   assign out = output_line;
 
    genvar i;
 
    // lower 8 FA
-   full_adder f_0(a[0], b[0], 0, s[0], out[0], c[1]);
+   full_adder f_0(a[0], b[0], 0, s[0], sum[0], c[1]);
    generate
       for (i = 1 i < 8; i = i + 1) begin
-         full_adder f_i(a[i], b[i], cin[i], out[i], c[i + 1]);
+         full_adder f_i(a[i], b[i], cin[i], sum[i], c[i + 1]);
       end
    endgenerate
 
@@ -203,10 +284,10 @@ module ADD_SAT_slice (a, b, s, out);
    MUX2 mx1(c[8], 0, select_and, carry_upper);
 
    // upper 8 FA
-   full_adder f_8(a[8], b[8], carry_upper, s[8], out[8], c[9]);
+   full_adder f_8(a[8], b[8], carry_upper, s[8], sum[8], c[9]);
    generate
       for (i = 9 i < 16; i = i + 1) begin
-         full_adder f_i(a[i], b[i], cin[i], out[i], c[i + 1]);
+         full_adder f_i(a[i], b[i], cin[i], sum[i], c[i + 1]);
       end
    endgenerate
 
@@ -214,10 +295,11 @@ module ADD_SAT_slice (a, b, s, out);
    xor_nand x1(c[7], c[8], ol),
             x2(c[15], c[16], oh);
 
-
-
-
-   
+   wire smallest, largest;
+   assign largest = 2'h7f;
+   assign smallest = 2'h80;
+   MUX4_8   mx4_8_1(sum[7:0], sum[7:0], smallest, largest, {ol, a[7]}, output_line[7:0]),
+            mx4_8_1(sum[15:8], sum[15:8], smallest, largest, {oh, a[15]}, output_line[15:8]);
 endmodule
 
 
@@ -225,10 +307,16 @@ endmodule
 
 // Structural for ALU
 module ALU (a, b, s, out);
-input [15:0] a, b; // 16-bit inputs
-input [1:0] s;     // 2-bit select  
-output [15:0] out; // 16-bit outputs
+   input [15:0] a, b; // 16-bit inputs
+   input [1:0] s;     // 2-bit select  
+   output [15:0] out; // 16-bit outputs
 
-// AND == 00, NOT == 01, ADD == 10, SAT == 11
+   // AND == 00, NOT == 01, ADD == 10, SAT == 11
+   wire [15:0] and_line, not_line, add_sat_line;
 
+   AND_ALU_slice and_slice(a, b, and_line);
+   NOT_ALU_slice not_slice(b, not_line);
+   ADD_SAT_slice add_sat_slice(a, b, s, add_sat_line);
+
+   MUX4_16 mx4_16_1(and_line, not_line, add_sat_line, add_sat_line, s, out);
 endmodule
