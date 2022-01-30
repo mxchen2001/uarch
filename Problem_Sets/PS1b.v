6427e0ca915f685f1d6e8881d8cbacee1f04bc2d
Xige M Chen <xchen2@mario.ece.utexas.edu>
2022-01-29 20:52:32 -0600

    finish implementation

--- Problem_Sets/PS1b.v
+++ Problem_Sets/PS1b.v
@@ -2,62 +2,105 @@ module TOP;
 
    reg [15:0] a, b;
    reg [1:0] s;
-   wire [15:0] out;
+   reg clk = 1;
+   wire [15:0] muxa_out, muxb_out, reg_in, reg_out;
+   reg muxa_s, muxb_s;
    
    initial
    begin
+
+      muxa_s = 1'b1;
+      muxb_s = 1'b1;
+
+      // #25
+
+      // AND
       a = 16'h0f0f;
       b = 16'hf0f0;
       s = 2'b00;
 
-      #15
+      #14
 
       a = 16'hcccc;
       b = 16'h6666;
       s = 2'b00;
 
-      #15
+      #14
 
-      a = 16'hcccc;
-      b = 16'h6666;
-      s = 2'b11;
-
-      #15
-
+      // NOT
       a = 16'h0f0f;
       b = 16'hcc77;
       s = 2'b01;
 
-      #15
+      #14
 
-      a = 16'h1234;
-      b = 16'haaaa;
+      a = 16'hffff;
+      b = 16'h0000;
+      s = 2'b01;
+
+      #14
+
+      // ADD
+      a = 16'hcccc;
+      b = 16'h6666;
       s = 2'b10;
 
-      #15
+      #14
 
-      a = 16'h1282;
-      b = 16'h1282;
+      a = 16'hffff;
+      b = 16'h0001;
+      s = 2'b10;
+
+      #14
+
+      // SAT
+      a = 16'hcccc;
+      b = 16'h6666;
+      s = 2'b11;
+
+      #14
+
+      a = 16'h7282;
+      b = 16'h7282;
+      s = 2'b11;
+
+      #14
+
+      // AND
+      a = 16'h0001;
+      b = 16'h0001;
+      s = 2'b00;
+
+      #14
+
+      // Shift 
+      muxa_s = 0;
+      muxb_s = 0;
       s = 2'b11;
    end
    
    // Run simulation for 15 ns.  
-   initial #150 $finish;
+   initial #3000 $finish;
    
    // Dump all waveforms to d_latch.dump.vpd
    initial
    begin
-      //$dumpfile ("d_latch.dump");
+      //$dumpfile ("PS1b.dump");
       //$dumpvars (0, TOP);
       $vcdplusfile("PS1b.dump.vpd");
       $vcdpluson(0, TOP); 
    end // initial begin
+
+   register_16 ALUout(reg_in, reg_out, 1'b1, clk);
+
+   MUX2_16  muxa(reg_out, a, muxa_s, muxa_out),
+            muxb(reg_out, b, muxb_s, muxb_out);
    
-   // always @(posedge d)
-   //    $strobe ("at time %0d, wen = %b", $time, we);
+   ALU alu(muxa_out, muxb_out, s, reg_in);
    
-   ALU alu(a, b, s, out);
-   
+
+   always
+      #7 clk = ~clk;
 endmodule
 
 //-----------------------------------------------------
@@ -82,6 +125,42 @@ module d_latch (d, q, qbar, wen);
           nand3 (q, s, qbar),
           nand4 (qbar, r, q);
    
+endmodule
+
+module d_flip_flop (d, q, clk);
+   input d, clk;
+   output q;
+
+   wire clk_bar;
+   wire q_master, qbar_master, qbar_slave;
+
+   not_nand nn1(clk, clk_bar);
+
+   // master latch, latches on falling edge
+   d_latch  m_latch(d, q_master, qbar_master, clk_bar);
+   // slave latch
+   d_latch  s_latch(q_master, q, qbar_slave, clk);
+   
+endmodule
+
+module register_16(d, q, enable, clk);
+   input [15:0] d;
+   input enable, clk;
+   output [15:0] q;
+
+   wire [15:0] q_bar;
+
+   wire wen;
+
+   and_nand an1(enable, clk, wen);
+
+   // set 16 D flip-flops
+   genvar i;
+   generate
+      for (i = 0; i < 16; i = i + 1) begin : reg_ff_gen
+         d_flip_flop dff(d[i], q[i], clk);
+      end
+   endgenerate
 endmodule
 
 // Components
@@ -145,8 +224,11 @@ module full_adder (a, b, cin, s, cout);
             n2(n_ac, a, cin),
             n3(n_bc, b, cin);
 
-   // 3 input OR structure
-   nand3$   n4(cout, n_ab, n_ac, n_bc);
+   // 3 input OR-nand structure
+   wire inter1, inter1_bar;
+   nand2$   n4(inter1, n_ab, n_ac),
+            n5(inter1_bar, inter1, inter1),
+            n6(cout, inter1_bar, n_bc);
 endmodule
 
 module MUX2 (i0, i1, s, o);
@@ -160,6 +242,26 @@ module MUX2 (i0, i1, s, o);
    nand2$   n1(inter1, i0, sbar),
             n2(inter2, i1, s),
             n3(o, inter1, inter2);
+endmodule
+
+module MUX2_16 (i0, i1, s, o);
+   input [15:0] i0, i1;
+   input s;
+   output [15:0] o;
+
+   wire [15:0] inter1, inter2;
+   wire sbar;
+
+   not_nand nn1(s, sbar);
+
+   genvar i;
+   generate
+      for (i = 0; i < 16; i = i + 1) begin : mux2_gate_gen
+         nand2$   n1(inter1[i], i0[i], sbar),
+                  n2(inter2[i], i1[i], s),
+                  n3(o[i], inter1[i], inter2[i]);
+      end
+   endgenerate
 endmodule
 
 module MUX4_16 (i0, i1, i2, i3, s, o);
@@ -289,7 +391,7 @@ module ADD_SAT_slice (a, b, s, add_out, sat_out);
    genvar i;
 
    // lower 8 FA
-   full_adder f_0(a[0], b[0], 0, sum[0], carry[1]);
+   full_adder f_0(a[0], b[0], 1'b0, sum[0], carry[1]);
    generate
       for (i = 1; i < 8; i = i + 1) begin : gen1
          full_adder f_i(a[i], b[i], carry[i], sum[i], carry[i + 1]);
@@ -298,7 +400,7 @@ module ADD_SAT_slice (a, b, s, add_out, sat_out);
 
    wire select_and, carry_upper;
    and_nand an1(s[0], s[1], select_and);
-   MUX2 mx1(carry[8], 0, select_and, carry_upper);
+   MUX2 mx1(carry[8], 1'b0, select_and, carry_upper);
 
    // upper 8 FA
    full_adder f_8(a[8], b[8], carry_upper, sum[8], carry[9]);
@@ -312,6 +414,7 @@ module ADD_SAT_slice (a, b, s, add_out, sat_out);
    xor_nand x1(carry[7], carry[8], ol),
             x2(carry[15], carry[16], oh);
 
+   // Saturation Mux
    MUX4_8   mx4_8_1(sum[7:0], sum[7:0], 8'h7f, 8'h80, {ol, a[7]}, sat_out[7:0]),
             mx4_8_2(sum[15:8], sum[15:8], 8'h7f, 8'h80, {oh, a[15]}, sat_out[15:8]);
 
